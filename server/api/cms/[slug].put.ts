902981e3a5dd673f144b7a11fb2ca66bbaf7feef
krs1977 <senthil.karuppaiah@hitachivantara.com>
2023-05-09 08:00:55 -0400

    fix for unstorage/cms save

--- server/api/cms/[slug].put.ts
+++ server/api/cms/[slug].put.ts
@@ -1,11 +1,13 @@
 const storage = useStorage();
-const config = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
   const { slug } = event.context.params || {};
   // Force being a string (CF workers always returns a Buffer)
-  const body = (await readRawBody(event, 'utf8'))?.toString();
-  console.log('saving into storage with key =', `${slug}`);
-  await storage.setItem(`${slug}`, body);
+  let body = (await readRawBody(event, 'utf8'))?.toString();
+  let mount = `${process.env.TEMPLRJS_CMS_STORAGE_MOUNT}` || 'fs_cms'; //File System mount i.e. .data/cms/*
+  if (typeof body !== 'string') {
+    body = '# The page you are looking for does not exist';
+  }
+  await storage.getMount(mount).driver.setItem(`${slug}`, body);
   return { slug, body };
 });
