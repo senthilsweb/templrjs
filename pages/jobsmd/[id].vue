c67bc3f546c4beb645ab73c223ef51e161b3b974
prithivi <prithiviiiiiiii@gmail.com>
2023-08-20 22:37:50 +0530

    test commit

--- pages/jobsmd/[id].vue
+++ pages/jobsmd/[id].vue
@@ -7,7 +7,7 @@
             <h1 class="font-semibold leading-6 text-gray-900 text-2xl">{{ job.title }}</h1>
             <h1 class="font-normal leading-6 text-gray-400 text-base">{{ job.location }}</h1>
           </div>
-          <p class="mt-6 max-w-4xl text-sm font-medium text-gray-500">{{ job.description }}</p>
+          <p class="mt-6 max-w-4xl text-sm font-medium text-gray-500">{{job.description}}</p>
         </div>
       </NuxtLink>
     </div>
