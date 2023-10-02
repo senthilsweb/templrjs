60545392dcc0d568a602aea027851491a5d78c00
Senthilnathan Karuppaiah <senthilsweb@gmail.com>
2023-10-02 01:55:54 +0000

    Uopdated the resume tempate

--- web/layouts/landing.vue
+++ web/layouts/landing.vue
@@ -14,7 +14,7 @@ onMounted(() => {
     <HeaderSpotLight v-if="showHeader" class="pt-6 pl-2 pr-2 sm:pl-0 sm:pr-0"/>
     <div class="mx-auto">
       <div class="min-h-screen relative">
-        <div class="h-full relative mt-16">
+        <div class="h-full relative" :class="!showHeader ? 'mt-0' : 'mt-16'">
           <slot />
         </div>
       </div>
--- web/pages/resume/index.vue
+++ web/pages/resume/index.vue
@@ -2,13 +2,12 @@
   <NuxtLayout name="landing">
     <div v-if="showHeader" class="border-b mt-4 border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
       <h2 class="text-2xl font-semibold text-slate-600 pl-4 sm:pl-0">Resume</h2>
-
       <div class="mt-3 flex sm:ml-4 sm:mt-0 hidden sm:flex">
         <button type="button" @click="generatePDF" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Download</button>
       </div>
     </div>
     <div class="p-4" id="resume" name="resume">
-      <div class="relative mt-8 flex items-center gap-x-4">
+      <div class="relative mt-0 flex items-center gap-x-4">
         <img v-if="data.basics.picture" :src="data.basics.picture" :alt="data.basics.name" class="h-28 w-28 flex-none rounded-lg bg-gray-800 object-cover" />
         <div class="text-sm leading-6">
           <p class="font-semibold text-2xl text-zinc-800 dark:text-zinc-100">
@@ -33,12 +32,12 @@
 
       <p class="text-sm text-zinc-600 dark:text-zinc-400 mt-2">{{ data.basics.summary }}</p>
       <!-- Professional Synopsis -->
-      <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Professional Synopsis</h2>
+      <h2 class="text-2xl font-semibold text-gray-600 mt-4">Professional Synopsis</h2>
       <ul class="list-disc text-sm text-zinc-600 dark:text-zinc-400 pl-6 mt-2">
         <li v-for="item in data.professionalSynopsis" :key="item" v-html="item"></li>
       </ul>
       <!-- Core Competencies -->
-      <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Core Competencies</h2>
+      <h2 class="text-2xl font-semibold text-gray-600 mt-4">Core Competencies</h2>
       <div class="mt-2 grid grid-cols-2 gap-x-2 md:grid-cols-4 md:gap-y-0">
         <div v-for="item in data.coreCompetencies" :key="item" class="group relative">
           <h3 class="mt-1 text-md font-semibold text-zinc-400 dark:text-zinc-400">
@@ -51,7 +50,7 @@
       </div>
 
       <!-- Technical Competencies -->
-      <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Technical Competencies</h2>
+      <h2 class="text-2xl font-semibold text-gray-600 mt-4">Technical Competencies</h2>
       <div class="mt-2 grid grid-cols-2 gap-x-2 md:grid-cols-4 md:gap-y-0">
         <div v-for="item in data.technicalCompetencies" :key="item" class="group relative">
           <h3 class="mt-1 text-sm font-semibold text-zinc-400 dark:text-zinc-400">
@@ -64,22 +63,22 @@
       </div>
 
       <!-- Certifications Section -->
-      <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Certifications</h2>
+      <h2 class="text-2xl font-semibold text-gray-600 mt-4">Certifications</h2>
       <ul class="list-disc text-sm text-zinc-600 dark:text-zinc-400 pl-6 mt-2">
         <li v-for="item in data.certifications" :key="item">{{ item.name }}</li>
       </ul>
       <!-- Notable Accomplishments -->
       <div v-if="data.notableAccomplishments" class="mt-4">
-        <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Notable Accomplishments</h2>
+        <h2 class="text-2xl font-semibold text-gray-600 mt-4">Notable Accomplishments</h2>
         <ul class="list-disc text-sm text-zinc-600 dark:text-zinc-400 pl-6 mt-2">
           <li v-for="item in data.notableAccomplishments" :key="item">{{ item }}</li>
         </ul>
       </div>
       <!-- Work Experience -->
       <div class="mt-4">
-        <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Work Experience</h2>
+        <h2 class="text-2xl font-semibold text-gray-600 mt-4">Work Experience</h2>
         <div v-for="job in data.work" :key="job.company" class="mt-4 pb-2">
-          <h3 class="text-xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">{{ job.company }} - {{ job.position }}</h3>
+          <h3 class="font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">{{ job.company }} - {{ job.position }}</h3>
           <p class="font-semibold tracking-tight text-zinc-400 dark:text-zinc-100">{{ job.location }}</p>
           <p class="text-xs mt-1 tracking-tight text-zinc-400 dark:text-zinc-100"><Icon name="icon-park-twotone:calendar-dot" class="w-4 h-4 inline-block mr-1" /> {{ job.startDate }} - {{ job.endDate }}</p>
           <p class="mt-2 tracking-tight text-sm text-zinc-600 dark:text-zinc-400">{{ job.summary }}</p>
@@ -87,9 +86,9 @@
       </div>
       <!-- Volunteering Experience -->
       <div v-if="data.volunteer" class="mt-4">
-        <h2 class="text-3xl font-spotlight text-gray-600 mt-4">Volunteering</h2>
+        <h2 class="text-2xl font-semibold text-gray-600 mt-4">Volunteering</h2>
         <div v-for="job in data.volunteer" :key="job.organization" class="mt-4 pb-2">
-          <h3 class="text-xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">{{ job.organization }} - {{ job.position }}</h3>
+          <h3 class="font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">{{ job.organization }} - {{ job.position }}</h3>
           <p class="font-semibold tracking-tight text-zinc-400 dark:text-zinc-100">{{ job.location }}</p>
           <p class="text-xs mt-1 tracking-tight text-zinc-400 dark:text-zinc-100"><Icon name="icon-park-twotone:calendar-dot" class="w-4 h-4 inline-block mr-1" /> {{ job.startDate }} - {{ job.endDate }}</p>
           <p class="mt-2 tracking-tight text-sm text-zinc-600 dark:text-zinc-400">{{ job.summary }}</p>
